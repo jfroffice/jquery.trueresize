bde9f7c117c888f45547cf4f316fe4cd5790190f
John Fischer <me@jfroffice.me>
2013-02-24 19:50:30 +0100

    change height and width at the same time

--- go.js
+++ go.js
@@ -10,9 +10,12 @@ $(function() {
         var width = $frame.css('width').replace('px', '');
         width = (width == 900) ? 550 : 900;
 
+        var height = $frame.css('height').replace('px', '');
+        height = (height == 600) ? 400 : 600;
+
         var duration = $duration.val();
 
-        $frame.animate({ width: width }, { duration: duration });
+        $frame.animate({ width: width, height: height }, { duration: duration });
     });
 
 });
