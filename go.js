57ec3ce4b8261a8d3674d1692847daadb3ff6ca4
John Fischer <me@jfroffice.me>
2013-02-24 19:30:45 +0100

    fix px problem

--- go.js
+++ go.js
@@ -4,7 +4,7 @@ $(function() {
 
     $('.btn').click(function() {
 
-        var width = $frame.css('width');
+        var width = $frame.css('width').replace('px', '');
         width = (width > 900) ? 600 : 900;
 
         $frame.animate({ width:width }, { duration:2000 });
