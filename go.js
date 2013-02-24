f7245ab4db6732c4ff648aa21c342a47973a1bf6
John Fischer <me@jfroffice.me>
2013-02-24 19:32:08 +0100

    min time

--- go.js
+++ go.js
@@ -5,9 +5,9 @@ $(function() {
     $('.btn').click(function() {
 
         var width = $frame.css('width').replace('px', '');
-        width = (width > 900) ? 600 : 900;
+        width = (width == 900) ? 600 : 900;
 
-        $frame.animate({ width:width }, { duration:2000 });
+        $frame.animate({ width:width }, { duration:1000 });
     });
 
 });
