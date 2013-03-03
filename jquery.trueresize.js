40eb38cc03c9840284095528023e5fcad84bea99
jfroffice <me@jfroffice.me>
2013-03-03 12:36:45 +0100

    more optims

--- jquery.trueresize.js
+++ jquery.trueresize.js
@@ -3,13 +3,11 @@
     var timer;
 
     $(window).resize(function() {
-        (function() {
-            timer && clearTimeout(timer);
-            timer = setTimeout(function() {
-                timer = null;
-                $(window).trigger('trueresize');
-            }, 20);
-        })();
+        timer && clearTimeout(timer);
+        timer = setTimeout(function() {
+            timer = null;
+            $(window).trigger('trueresize');
+        }, 20);
     });
 
 })(jQuery);
